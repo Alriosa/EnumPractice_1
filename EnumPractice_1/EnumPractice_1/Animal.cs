3d9a03a9ddef3b6ea0f80d9d6114a97683584547
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-04-23 00:23:38 -0600

    1

---
+++ EnumPractice_1/EnumPractice_1/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice_1
+{
+    public class Animal
+    {
+        public string name;
+        public int age;
+        public float happiness;
+    }
+}
---
+++ EnumPractice_1/EnumPractice_1/MainClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static EnumPractice_1.Dog;
+
+namespace EnumPractice_1
+{
+
+    enum Breed
+    {
+        Bulldog,
+        Boxer,
+        Chihuahua,
+        Briard
+    };
+
+    class MainClass
+    {
+
+        class Animal
+        {
+            public string name;
+            public int age;
+            public float happiness;
+        }
+
+        class Dog : Animal
+        {
+            public Breed breed;
+
+            public Dog(string name, int age, float happiness, Breed breed)
+            {
+                name = name;
+                age = age;
+                happiness = happiness;
+                breed = breed;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+       
+
+
+
+        Console.WriteLine("Name" + name);
+        Console.WriteLine("Age" + age);
+        Console.WriteLine("Happiness" + happiness);
+         
+
+        }
+    }
+}
